bf22dcaac3d2f1de1c10bc9dae5ca5d8dd1160f4
Elif <elifozer173@gmail.com>
2021-12-15 20:35:50 +0300

    Projects are initialized User class is added to core project

---
+++ ComplaintManagement.Core/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ComplaintManagement.Core
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string Username { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+
+        public User(string username)
+        {
+            Username = username;
+            CreatedOn = DateTime.UtcNow;
+        }
+    }
+}
